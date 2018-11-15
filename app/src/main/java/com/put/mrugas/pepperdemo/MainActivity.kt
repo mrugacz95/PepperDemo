f2886c2a524ac2605e27195ae27056c75153b66d
Marcin Mrugas <marcin.mrugas@snow.dog>
2018-11-16 00:03:36 +0100

    Basic pepper activity

--- app/src/main/java/com/put/mrugas/pepperdemo/MainActivity.kt
+++ app/src/main/java/com/put/mrugas/pepperdemo/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.put.mrugas.pepperdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.aldebaran.qi.sdk.QiContext
+import com.aldebaran.qi.sdk.QiSDK
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks
+import com.aldebaran.qi.sdk.design.activity.RobotActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RobotActivity(), RobotLifecycleCallbacks {
+    override fun onRobotFocusGained(qiContext: QiContext?) {
+
+    }
+
+    override fun onRobotFocusLost() {
+
+    }
+
+    override fun onRobotFocusRefused(reason: String?) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        QiSDK.register(this, this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDestroy() {
+        QiSDK.unregister(this, this)
+        super.onDestroy()
     }
 }
